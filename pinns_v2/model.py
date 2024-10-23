571591d699981ef2ce8f1ca6ab8512e78d2d19b4
Simone Sangiorgio <simone.sangiorgio2000@gmail.com>
2024-10-23 14:43:41 +0200

    Add files via upload

---
+++ pinns_v2/model.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from pinns_v2.rff import GaussianEncoding
+from collections import OrderedDict
+import math
+
+
+class ModifiedMLP(nn.Module):
+    def __init__(self, layers, activation_function, hard_constraint_fn=None, p_dropout=0.2, encoding=None) -> None:
+        super(ModifiedMLP, self).__init__()
+
+        self.layers = layers
+        self.activation = activation_function
+        self.encoding = encoding
+        if encoding != None:
+            encoding.setup(self)
+        
+        self.U = torch.nn.Sequential(nn.Linear(self.layers[0], self.layers[1]), self.activation())
+        self.V = torch.nn.Sequential(nn.Linear(self.layers[0], self.layers[1]), self.activation())
+
+        layer_list = nn.ModuleList()        
+        for i in range(0, len(self.layers)-2):
+            layer_list.append(
+                nn.Linear(layers[i], layers[i+1])
+            )
+            layer_list.append(self.activation())
+            layer_list.append(Transformer())
+            layer_list.append(nn.Dropout(p = p_dropout))
+        self.hidden_layer = layer_list
+        self.output_layer = nn.Linear(self.layers[-2], self.layers[-1])
+
+        self.hard_constraint_fn = hard_constraint_fn
+        
+
+    def forward(self, x):
+        orig_x = x
+        if self.encoding != None:
+            x = self.encoding(x)
+
+        U = self.U(orig_x)
+        V = self.V(orig_x)
+
+        output = x
+        for i in range(0, len(self.hidden_layer), 4):
+            output = self.hidden_layer[i](output) #Linear
+            output = self.hidden_layer[i+1](output) #Activation
+            output = self.hidden_layer[i+2](output, U, V) #Transformer
+            output = self.hidden_layer[i+3](output) #Dropout
+        output = self.output_layer(output)
+
+        if self.hard_constraint_fn != None:
+            output = self.hard_constraint_fn(orig_x, output)
+
+        return output
+
+class MLP(nn.Module):
+    def __init__(self, layers, activation_function, hard_constraint_fn=None, p_dropout=0.2, encoding=None) -> None:
+        super(MLP, self).__init__()
+
+        self.layers = layers
+        self.activation = activation_function
+        self.encoding = encoding
+        if encoding != None:
+            encoding.setup(self)
+
+        layer_list = list()        
+        for i in range(len(self.layers)-2):
+            layer_list.append(
+                ('layer_%d' % i, nn.Linear(layers[i], layers[i+1]))
+            )
+            layer_list.append(('activation_%d' % i, self.activation()))
+            layer_list.append(('dropout_%d' % i, nn.Dropout(p = p_dropout)))
+        layer_list.append(('layer_%d' % (len(self.layers)-1), nn.Linear(self.layers[-2], self.layers[-1])))
+
+        self.mlp = nn.Sequential(OrderedDict(layer_list))
+
+        self.hard_constraint_fn = hard_constraint_fn
+
+    def forward(self, x):
+        orig_x = x
+        if self.encoding != None:
+            x = self.encoding(x)
+
+        output = self.mlp(x)
+
+        if self.hard_constraint_fn != None:
+            output = self.hard_constraint_fn(orig_x, output)
+
+        return output
+
+
+class Sin(nn.Module):
+  def __init__(self):
+    super(Sin, self).__init__()
+
+  def forward(self, x):
+    return torch.sin(x)
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+    
+    def forward(self, x, U, V):
+        return torch.multiply(x, U) + torch.multiply(1-x, V)
+        #return torch.nn.functional.linear(torch.multiply(x, U) + torch.multiply((1-x), V), self.weight, self.bias)
+
+class FactorizedLinear(nn.Module):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None, sigma = 0.1, mu = 1.0):
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = torch.empty((out_features, in_features), **factory_kwargs)
+        self.weight = nn.init.xavier_normal_(self.weight)
+        self.s = (torch.randn(self.in_features) * sigma) + mu
+        self.s = torch.exp(self.s)
+        self.v = self.weight/self.s
+        self.s = nn.parameter.Parameter(self.s)
+        self.v = nn.parameter.Parameter(self.v)
+        if bias:
+            self.bias = nn.parameter.Parameter(torch.empty(out_features, **factory_kwargs))
+        else:
+            self.register_parameter("bias", None)
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        if self.bias is not None:
+            fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+            nn.init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return nn.functional.linear(input, self.s*self.v, self.bias)
+
+    def extra_repr(self) -> str:
+        return f"in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}"
+
+
+class FactorizedModifiedLinear(FactorizedLinear):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True, device=None, dtype=None):
+        super().__init__(in_features, out_features, bias, device=device, dtype=dtype)
+    
+    def forward(self, x, U , V):
+        return torch.nn.functional.linear(torch.multiply(x, U) + torch.multiply((1-x), V), self.s*self.v, self.bias)
+
+
+					
---
+++ pinns_v2/train.py
@@ -0,0 +1,148 @@
+import torch
+from torch.func import vmap
+import numpy as np
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import os
+import json
+import gc
+
+
+train_loss = []  # To store losses
+test_loss = []
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def train(data, output_to_file = True):  
+    name = data.get("name", "main")
+    model = data.get("model")
+    epochs = data.get("epochs")
+    batchsize = data.get("batchsize")
+    optimizer = data.get("optimizer")
+    scheduler = data.get("scheduler")
+    component_manager = data.get("component_manager")
+    additional_data = data.get("additional_data")
+
+    if output_to_file:
+        current_file = os.getcwd()
+        output_dir = os.path.join(current_file, "output", name)
+        
+        if os.path.exists(output_dir):
+            counter = 1
+            while True:
+                output_dir = os.path.join(current_file, "output", name+"_"+str(counter))
+                if not os.path.exists(output_dir):
+                    break
+                else:
+                    counter +=1
+                    
+        model_dir = os.path.join(output_dir, "model")
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+            
+
+        model_path = os.path.join(model_dir, f"model.pt")
+        file_path = f"{output_dir}/train.txt"
+
+        params_path = f"{output_dir}/params.json"
+        params = {
+            "name": name,
+            "model": str(model),
+            "epochs": epochs,
+            "batchsize": batchsize,
+            "optimizer": str(optimizer),
+            "scheduler": str(scheduler.state_dict()) if scheduler!=None else "None",
+            "modules": str(component_manager)
+        } 
+        if additional_data != None:
+            params["additionalData"] = additional_data
+        fp = open(params_path, "w", newline='\r\n') 
+        json.dump(params, fp)
+        fp.close()  
+
+
+    #for temporal causality weights
+    model = model.to(device)
+
+    for epoch in range(epochs):
+        model.train(True)
+        train_losses = []
+        for i in range(component_manager.number_of_iterations(train = True)):
+            l = component_manager.apply(model, train = True)
+            l.backward()    
+            optimizer.step() 
+            optimizer.zero_grad()
+
+            train_losses.append(l.item())
+
+            if i % 10 ==0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.10f}'.format(
+                    epoch, i, component_manager.number_of_iterations(train = True),
+                    100. * i / component_manager.number_of_iterations(train = True), l.item()))
+                
+                # Save to log file
+                #log_file.write('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.10f}\n'.format(
+                #    epoch, batch_idx, int(len(dataloader.dataset)/batchsize),
+                #    100. * batch_idx / len(dataloader), loss.item()))
+        
+        train_loss.append(np.average(train_losses))
+
+        model.eval()
+        validation_losses = []
+        for i in range(component_manager.number_of_iterations(train = False)):
+            l = component_manager.apply(model, train = False)
+            validation_losses.append(l.item())
+
+            del l
+            gc.collect()
+            
+            if i % 10 == 0:
+                print('Validation Epoch: {} \tLoss: {:.10f}'.format(
+                    epoch, np.average(validation_losses)))
+            
+        
+        test_loss.append(np.average(validation_losses))
+                
+        if output_to_file and epoch % 20 == 0:
+            epoch_path = os.path.join(model_dir, f"model_{epoch}.pt")
+            torch.save(model, epoch_path)
+        
+        if scheduler != None:
+            scheduler.step()
+
+        torch.cuda.empty_cache()
+    
+    # Save the model
+    if output_to_file:
+        torch.save(model, model_path)
+        
+        plt.plot(train_loss)
+        plt.xlabel('Iterations')
+        plt.ylabel('Loss')
+        plt.title('Training Loss')
+        plt.savefig(f'{output_dir}/training_loss.png')
+        plt.clf()
+        plt.plot(train_loss)
+        plt.plot(test_loss)
+        plt.xlabel('Iterations')
+        plt.ylabel('Loss')
+        plt.title('Training and Validation Loss')
+        plt.savefig(f'{output_dir}/train_and_test_loss.png')
+        plt.clf()
+        label = ["Residual loss", "IC loss"]
+        residual_losses = component_manager.search("Residual", train = False).loss.history
+        ic_losses = component_manager.search("IC", train = False).loss
+        plt.plot(residual_losses)
+        for i in range(len(ic_losses)):
+            plt.plot(ic_losses[i].history)
+            label.append("IC_loss_"+str(i))
+        plt.legend(label)
+        plt.xlabel('Iterations')
+        plt.ylabel('Loss')
+        plt.title('Training Losses')
+        plt.savefig(f'{output_dir}/train_losses.png')
+        plt.show()
+
+    return np.min(test_loss)
